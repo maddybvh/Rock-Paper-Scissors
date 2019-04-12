aacc783ab558098656d02c1a4fe68634945e7be1
Maddy Bishop-Van Horn <maddy@savaslabs.com>
2019-04-12 16:41:59 -0400

    Adding stylized game version

--- README.md
+++ README.md
@@ -2,5 +2,3 @@
 
 The Odin Project curriculum
 https://www.theodinproject.com/courses/web-development-101/lessons/rock-paper-scissors?ref=lnav
-
-Play by going to https://maddybvh.github.io/Rock-Paper-Scissors. Open the console to see the results.
---
+++ script.js
@@ -0,0 +1,103 @@
+function computerPlay (){
+    x = Math.floor(3 * Math.random())
+    switch (x){
+        case 0:
+            return 'rock'
+            break;
+        case 1:
+            return 'paper'
+            break;
+        case 2:
+            return 'scissors'
+        }
+    }
+    
+
+
+function playRound (player, computer){
+    console.log('Computer: ' + computer);
+    let outcome = '';
+    if (player == computer) {
+        outcome = "It's a tie, try again."
+    }
+    else if (player == 'rock' && computer == 'scissors'){
+        outcome = 'You win!'
+    }
+    else if (player == 'rock' && computer == 'paper'){
+        outcome = 'You lose!'
+    }
+    else if (player == 'scissors' && computer == 'rock'){
+        outcome = 'You lose!'
+    }
+    else if (player == 'scissors' && computer == 'paper'){
+        outcome = 'You win!'
+    }
+    else if (player == 'paper' && computer == 'scissors'){
+        outcome = 'You lose!'
+    }
+    else if (player == 'paper' && computer == 'rock'){
+        outcome = 'You win!'
+    }
+    console.log(outcome)
+    return outcome
+}
+
+function keepScore(outcome, playerScore, computerScore){
+
+}
+
+let playerScore = 0;
+let computerScore = 0;
+let roundNumber = 1;
+
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    console.log(button.id);
+    computerChoice = computerPlay();
+    outcome = playRound(button.id, computerChoice);
+    displayOutcome (computerChoice, outcome)
+  });
+});
+
+function displayOutcome (computerChoice, outcome){
+    let string = 'Computer choses ' + computerChoice + '. ' + outcome
+    document.getElementById(1).innerHTML = string;
+
+    switch (true){
+        case (outcome == 'You win!'):
+            playerScore += 1;
+            break;
+        case (outcome == 'You lose!'):
+            computerScore += 1;
+            break;
+        case (outcome == "It's a tie, try again."):
+            roundNumber -= 1;
+    }
+    roundNumber += 1;
+
+    document.getElementById(2).innerHTML = playerScore;
+    document.getElementById(3).innerHTML = roundNumber;
+    document.getElementById(4).innerHTML = computerScore;
+    
+    if (playerScore > 5 || computerScore > 5){
+        crownChampion(playerScore, computerScore);
+    }
+
+
+    return playerScore, computerScore, roundNumber;
+}
+
+function crownChampion (playerScore, computerScore){
+    if (playerScore > 5){
+        document.getElementById(5).innerHTML = 'GAME OVER -- you win!'
+    }
+    else if (computerScore > 5){
+            document.getElementById(5).innerHTML = 'GAME OVER -- you lose!'
+            }
+    
+    if(confirm('Play again?')){
+        window.location.reload();  
+    }
+}
