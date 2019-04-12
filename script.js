e98b28f5db014ccfa8fbf0b4205b54eea804d9a4
maddybvh <maddybvh@gmail.com>
2019-04-12 16:07:58 -0400

    Add several files

---
+++ script.js
@@ -0,0 +1,102 @@
+function computerPlay (){
+    x = Math.floor(3 * Math.random())
+    switch (x){
+        case 0:
+            return 'rock'
+            break;
+        case 1:
+            return 'paper'
+            break;
+        case 2:
+            return 'scissors'
+        }
+    }
+    
+
+
+function playRound (player, computer){
+    console.log('Computer: ' + computer);
+    let outcome = '';
+    if (player == computer) {
+        outcome = "It's a tie, try again."
+    }
+    else if (player == 'rock' && computer == 'scissors'){
+        outcome = 'You win!'
+    }
+    else if (player == 'rock' && computer == 'paper'){
+        outcome = 'You lose!'
+    }
+    else if (player == 'scissors' && computer == 'rock'){
+        outcome = 'You lose!'
+    }
+    else if (player == 'scissors' && computer == 'paper'){
+        outcome = 'You win!'
+    }
+    else if (player == 'paper' && computer == 'scissors'){
+        outcome = 'You lose!'
+    }
+    else if (player == 'paper' && computer == 'rock'){
+        outcome = 'You win!'
+    }
+    console.log(outcome)
+    return outcome
+}
+
+function keepScore(outcome, playerScore, computerScore){
+
+}
+
+let playerScore = 0;
+let computerScore = 0;
+let roundNumber = 1;
+
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll('button');
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+
+  // and for each one we add a 'click' listener
+  button.addEventListener('click', (e) => {
+    console.log(button.id);
+    computerChoice = computerPlay();
+    outcome = playRound(button.id, computerChoice);
+    displayOutcome (computerChoice, outcome)
+  });
+});
+
+
+function displayOutcome (computerChoice, outcome){
+    let string = 'Computer choses ' + computerChoice + '. ' + outcome
+    document.getElementById(1).innerHTML = string;
+
+    switch (true){
+        case (outcome == 'You win!'):
+            playerScore += 1;
+            break;
+        case (outcome == 'You lose!'):
+            computerScore += 1;
+            break;
+        case (outcome == "It's a tie, try again."):
+            roundNumber -= 1;
+    }
+    roundNumber += 1;
+
+    document.getElementById(2).innerHTML = playerScore;
+    document.getElementById(3).innerHTML = roundNumber;
+    document.getElementById(4).innerHTML = computerScore;
+    crownChampion(playerScore, computerScore);
+
+    return playerScore, computerScore, roundNumber;
+}
+
+function crownChampion (playerScore, computerScore){
+    if (playerScore == 5){
+        document.getElementById(5).innerHTML = 'Game over -- you win!'
+    }
+    else if (computerScore == 5){
+            document.getElementById(5).innerHTML = 'Game over -- you lose!'
+        }
+}
+
+
